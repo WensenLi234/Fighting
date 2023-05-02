ca6478bbb303383ed947b2cb577d0b5fb7b774ee
unknown <BT_4N2_20@BKTECH.LOCAL>
2023-05-02 11:59:37 -0400

    Initial commit

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Character {
+    String name;
+    int health;
+    ArrayList<Move> moveList;
+    public Character() {
+
+    }
+}
---
+++ src/Move.java
@@ -0,0 +1,5 @@
+public class Move {
+    Animation animation;
+    Hitbox hitbox;
+
+}
