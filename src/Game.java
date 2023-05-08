5e9f084ebcc61c16c59b7a661b71cfcb5c3414d1
BT_4N2_20 <wli9204@bths.edu>
2023-05-08 10:56:20 -0400

    e

--- src/Game.java
+++ src/Game.java
@@ -8,13 +8,13 @@ import java.util.Hashtable;
 
 public class Game extends JFrame implements KeyListener {
     private final Hashtable<String, Integer> keyBinds = new Hashtable<String, Integer>();
-    private static FileReader reader;
+    private static BufferedReader reader;
     private JFrame frame;
 
 
     static {
         try {
-            reader = new FileReader("src/Save");
+            reader = new BufferedReader("src/Save");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -31,15 +31,20 @@ public class Game extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         Integer key = e.getKeyCode();
-        for(String i : keyBinds.keySet()) {
-            if(keyBinds.get(i).equals(key)) {
-                // lol
+        for(String i: keyBinds.keySet()) {
+            if(key.equals(keyBinds.get(i))) {
+                System.out.println("MY HEALTH IS BACK TO TIP TOP SHAPE");
             }
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        Integer key = e.getKeyCode();
+        for(String i: keyBinds.keySet()) {
+            if(key.equals(keyBinds.get(i))) {
+                System.out.println("DIEEEEEE");
+            }
+        }
     }
 }
