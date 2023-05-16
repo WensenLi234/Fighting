c85fa9d0dc7529081c038eba92cfdbd53276d342
BT_4N2_20 <wli9204@bths.edu>
2023-05-16 12:23:17 -0400

    e

--- src/Character.java
+++ src/Character.java
@@ -4,7 +4,11 @@ public class Character {
     private String name;
     private int health;
     private ArrayList<Move> moveList;
-    public Character() {
-
+    private ImageComponent sprite;
+    public Character(String name, int health, ArrayList<Move> moveList, ImageComponent sprite) {
+        this.name = name;
+        this.health = health;
+        this.moveList = moveList;
+        this.sprite = sprite;
     }
 }
