2696539654cd4843bb971eff2e18e9c5e9dfe4f8
BT_4N2_20 <wli9204@bths.edu>
2023-05-22 12:23:16 -0400

    Started working on class. Rectangle object needs to be visible when drawn using the drawComponent() method.

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
-public class Hitbox extends JFrame {
-    private Rectangle box;
-    public double[] coordinates;
-    public Hitbox(double positionX, double positionY) {
-
+public class Hitbox extends JComponent {
+    private Rectangle2D rectangle;
+    public Hitbox(int x, int y, int width, int height) {
+        rectangle = new Rectangle(x, y, width, height);
     }
     public Hitbox(Rectangle rectangle) {
+        this.rectangle = rectangle;
 
+    }
+    public void paintComponent(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setColor(Color.RED);
+        g2D.draw(rectangle);
     }
 }
