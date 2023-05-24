34930254312c954bb4f8e39d3d7a3cd6b6de8dd2
BT_4N2_20 <wli9204@bths.edu>
2023-05-24 12:29:12 -0400

    Made the subclass of GUIElement. Changes made to adjust accordingly.

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -2,18 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
-public class Hitbox extends JComponent {
+public class Hitbox extends GUIElement implements CollisionListener {
     private Rectangle2D rectangle;
-    public Hitbox(int x, int y, int width, int height) {
+    private int numHits;
+    private int maxHits;
+    public Hitbox(int x, int y, int width, int height, int maxHits) {
+        super(x, y, width, height);
+
         rectangle = new Rectangle(x, y, width, height);
     }
     public Hitbox(Rectangle rectangle) {
+        super(rectangle.width, rectangle.height, rectangle.x, rectangle.y);
         this.rectangle = rectangle;
-
     }
-    public void paintComponent(Graphics g) {
+    public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
         g2D.setColor(Color.RED);
         g2D.draw(rectangle);
+        g2D.fill(rectangle);
     }
 }
