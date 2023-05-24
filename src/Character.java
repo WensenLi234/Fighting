f92588960e1905376869b4766ab54e6f243c0fb7
BT_4N2_20 <wli9204@bths.edu>
2023-05-24 12:29:39 -0400

    Changes made to adjust to the removal of ImageComponent

--- src/Character.java
+++ src/Character.java
@@ -1,11 +1,12 @@
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Character {
     private String name;
     private int health;
     private ArrayList<Move> moveList;
-    private ImageComponent sprite;
-    public Character(String name, int health, ArrayList<Move> moveList, ImageComponent sprite) {
+    private BufferedImage sprite;
+    public Character(String name, int health, ArrayList<Move> moveList, BufferedImage sprite) {
         this.name = name;
         this.health = health;
         this.moveList = moveList;
