9bd00aaf76163e4381e147074a860c53f558cbf4
BT_4N2_20 <wli9204@bths.edu>
2023-05-25 12:26:01 -0400

    Made new input variables numHits and maxHits and changed constructor to initialize those variables.

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -8,12 +8,15 @@ public class Hitbox extends GUIElement implements CollisionListener {
     private int maxHits;
     public Hitbox(int x, int y, int width, int height, int maxHits) {
         super(x, y, width, height);
-
+        numHits = 0;
+        this.maxHits = maxHits;
         rectangle = new Rectangle(x, y, width, height);
     }
-    public Hitbox(Rectangle rectangle) {
+    public Hitbox(Rectangle rectangle, int maxHits) {
         super(rectangle.width, rectangle.height, rectangle.x, rectangle.y);
         this.rectangle = rectangle;
+        numHits = 0;
+        this.maxHits = maxHits;
     }
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
