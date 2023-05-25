700487aa130cec8e9bb83afd14ee39ab62fbbf0b
BT_4N2_20 <wli9204@bths.edu>
2023-05-25 12:24:45 -0400

    Changes made to adjust to new InputHandler Class

--- src/Game.java
+++ src/Game.java
@@ -8,33 +8,22 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Hashtable;
 
-public class Game extends JFrame implements KeyListener {
-    private final Hashtable<String, Integer> keyBinds = new Hashtable<String, Integer>();
-    private static BufferedReader reader;
+public class Game extends JFrame {
+    private InputHandler inputHandler;
     private JFrame frame;
     private GamePanel panel;
-    private BufferedImage background;
 
-    static {
-        try {
-            reader = new BufferedReader(new FileReader("src/Save"));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public Game() throws IOException {
-        String[] keyBinds = reader.readLine().split(";");
-        for(String bind : keyBinds) {
-            this.keyBinds.put(bind.substring(0, bind.indexOf(":")), Integer.parseInt(bind.substring(bind.indexOf(":") + 1)));
-        }
         initializeGUI();
     }
+
     private void initializeGUI() throws IOException {
+        inputHandler = new InputHandler();
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.addKeyListener(this);
         frame.setResizable(false);
+        frame.addKeyListener(inputHandler);
         panel = new GamePanel(1000, 500);
         frame.add(panel);
         frame.pack();
@@ -48,28 +37,4 @@ public class Game extends JFrame implements KeyListener {
         g2D.drawImage(image, x, y, 300, 300, null);
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        Integer key = e.getKeyCode();
-        for(String i: keyBinds.keySet()) {
-            if(key.equals(keyBinds.get(i))) {
-                System.out.println(i);
-            }
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        Integer key = e.getKeyCode();
-        for(String i: keyBinds.keySet()) {
-            if(key.equals(keyBinds.get(i))) {
-
-            }
-        }
-    }
 }
