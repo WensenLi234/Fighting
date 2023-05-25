775383c925bcd262739bc693c6f128b12b953ea5
BT_4N2_20 <wli9204@bths.edu>
2023-05-25 12:26:01 -0400

    Made new input variables numHits and maxHits and changed constructor to initialize those variables.

--- src/Character.java
+++ src/Character.java
@@ -1,14 +1,11 @@
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class Character {
-    private String name;
-    private int health;
+public class Character extends Entity{
     private ArrayList<Move> moveList;
     private BufferedImage sprite;
     public Character(String name, int health, ArrayList<Move> moveList, BufferedImage sprite) {
-        this.name = name;
-        this.health = health;
+        super(name, health);
         this.moveList = moveList;
         this.sprite = sprite;
     }
