7dadb9e1a37480674c9b7da60983046dae8f1b71
BT_4N2_20 <wli9204@bths.edu>
2023-05-30 12:23:35 -0400

    Added a boolean stunned variable that will be checked to see if the character is able to perform a move.

--- src/Character.java
+++ src/Character.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 public class Character extends Entity{
     private ArrayList<Move> moveList;
     private BufferedImage sprite;
+    private boolean stunned;
     public Character(String name, int health, ArrayList<Move> moveList, BufferedImage sprite) {
         super(name, health);
         this.moveList = moveList;
         this.sprite = sprite;
+        this.stunned = false;
     }
 }
